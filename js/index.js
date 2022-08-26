5d7f049c63005ab61cbc549c347dc8fe78682fdd
Agustin <agsp3rs3o@gmail.com>
2022-08-26 03:56:46 -0300

    Finish proyect

--- js/index.js
+++ js/index.js
@@ -37,6 +37,14 @@ function fetchPokemons(offset, limit) {
 }
 
 function createPokemon(pokemon) {
+  const flipcard = document.createElement("div");
+  flipcard.classList.add("flip-card");
+
+  const cardContainer = document.createElement("div");
+  cardContainer.classList.add("card-container");
+
+  flipcard.appendChild(cardContainer);
+
   const card = document.createElement("div");
   card.classList.add("pokemon-block");
 
@@ -59,7 +67,49 @@ function createPokemon(pokemon) {
   card.appendChild(number);
   card.appendChild(name);
 
-  pokemonContainer.appendChild(card);
+  const cardBack = document.createElement("div");
+  cardBack.classList.add("pokemon-block-back");
+  cardBack.appendChild(progressBars(pokemon.stats));
+
+  cardContainer.appendChild(card);
+  cardContainer.appendChild(cardBack);
+  pokemonContainer.appendChild(flipcard);
+}
+
+function progressBars(stats) {
+  const statsContainer = document.createElement("div");
+  statsContainer.classList.add("stats-container");
+
+  for (let i = 0; i < 4; i++) {
+    const stat = stats[i];
+
+    const statPercent = stat.base_stat / 2 + "%";
+    const statContainer = document.createElement("stat-container");
+    statContainer.classList.add("stat-container");
+
+    const statName = document.createElement("p");
+    statName.textContent = stat.stat.name;
+
+    const progress = document.createElement("div");
+    progress.classList.add("progress");
+
+    const progressBar = document.createElement("div");
+    progressBar.classList.add("progress-bar");
+    progressBar.setAttribute("aria-valuenow", stat.base_stat);
+    progressBar.setAttribute("aria-valuemin", 0);
+    progressBar.setAttribute("aria-valuemax", 200);
+    progressBar.style.width = statPercent;
+
+    progressBar.textContent = stat.base_stat;
+
+    progress.appendChild(progressBar);
+    statContainer.appendChild(statName);
+    statContainer.appendChild(progress);
+
+    statsContainer.appendChild(statContainer);
+  }
+
+  return statsContainer;
 }
 
 function removeChildNodes(parent) {
